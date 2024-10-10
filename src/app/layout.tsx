74021ffd60b1109479118636a7607c49cade25c9
mrfaizee12 <faizee956@gmail.com>
2024-10-11 04:23:05 +0500

    updated again layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,19 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 // Commented out because these are not used currently
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
